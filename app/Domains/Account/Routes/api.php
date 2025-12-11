8bb14ee6ff471e10a4e86b3fead9accc12864c84
ZeinaAlbarre <zeinaalbarre3@gmail.com>
2025-12-11 17:18:15 +0300

    chore: update accountant api

--- app/Domains/Account/Routes/api.php
+++ app/Domains/Account/Routes/api.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\AccountController;
+use App\Domains\Account\Http\Controllers\AccountantController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/', [AccountController::class, 'index'])->middleware('permission:view-accounts');
-    Route::get('/my', [AccountController::class, 'myAccounts'])->middleware('permission:view-my-accounts');
-    Route::get('/portfolio/balance', [AccountController::class, 'portfolioBalance'])->middleware('permission:view-portfolio-balance');
-    Route::post('/', [AccountController::class, 'store'])->middleware('permission:create-account');
-    Route::get('/{account}', [AccountController::class, 'show'])->middleware('permission:view-account');
-    Route::put('/{account}', [AccountController::class, 'update'])->middleware('permission:update-account');
-    Route::patch('/{account}/state', [AccountController::class, 'changeState'])->middleware('permission:update-account');
+    Route::get('/', [AccountantController::class, 'index'])->middleware('permission:view-accounts');
+    Route::get('/my', [AccountantController::class, 'myAccounts'])->middleware('permission:view-my-accounts');
+    Route::get('/portfolio/balance', [AccountantController::class, 'portfolioBalance'])->middleware('permission:view-portfolio-balance');
+    Route::post('/', [AccountantController::class, 'store'])->middleware('permission:create-account');
+    Route::get('/{account}', [AccountantController::class, 'show'])->middleware('permission:view-account');
+    Route::put('/{account}', [AccountantController::class, 'update'])->middleware('permission:update-account');
+    Route::patch('/{account}/state', [AccountantController::class, 'changeState'])->middleware('permission:update-account');
 });
